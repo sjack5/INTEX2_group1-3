40534940f70659c7c39e80466c5131d18227b95b
Spencer Jacklin <sjacklin01@gmail.com>
2023-04-10 18:36:42 -0600

    Day 1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using INTEX2_group1_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEX2_group1_3.Controllers
+{
+    
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [RequireHttps]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [RequireHttps]
+        public IActionResult SupervisedPage()
+        {
+            return View();
+        }
+
+        [RequireHttps]
+        public IActionResult UnsupervisedPage()
+        {
+            return View();
+        }
+
+        [RequireHttps]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
