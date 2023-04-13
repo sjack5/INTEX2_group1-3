be82799e2c584e609369ae7d4cbfd33e1270aceb
Spencer Jacklin <sjacklin01@gmail.com>
2023-04-13 08:21:05 -0600

    Details

--- Models/Textile.cs
+++ Models/Textile.cs
@@ -10,13 +10,13 @@ namespace INTEX2_group1_3.Models
     public partial class Textile
     {
         public long Id { get; set; }
-        public string Locale { get; set; }
+        public string? Locale { get; set; }
         public int? Textileid { get; set; }
-        public string Description { get; set; }
-        public string Burialnumber { get; set; }
-        public string Estimatedperiod { get; set; }
+        public string? Description { get; set; }
+        public string? Burialnumber { get; set; }
+        public string? Estimatedperiod { get; set; }
         public DateTime? Sampledate { get; set; }
         public DateTime? Photographeddate { get; set; }
-        public string Direction { get; set; }
+        public string? Direction { get; set; }
     }
 }
